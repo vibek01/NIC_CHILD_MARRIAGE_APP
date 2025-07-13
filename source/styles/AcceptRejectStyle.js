d32146c3a66e7ce80811e844723ae736490a5e8f
vibek01 <vibekbin305@gmail.com>
2025-07-13 22:58:28 +0530

    updated styles of accept rejct style.js

--- source/styles/AcceptRejectStyle.js
+++ source/styles/AcceptRejectStyle.js
@@ -1,13 +1,19 @@
+// styles.js
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   row: {
     flexDirection: "row",
-    marginTop: 15,
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 16,
+    paddingHorizontal: 12,
   },
   status: {
     fontSize: 16,
     marginTop: 20,
     color: "#2c3e50",
+    fontWeight: "500",
+    textAlign: "center",
   },
 });
