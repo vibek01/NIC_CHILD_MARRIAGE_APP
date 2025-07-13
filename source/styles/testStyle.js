01d65cf357fb72608bb639e97bcb32b18b55f27c
vibek01 <vibekbin305@gmail.com>
2025-07-13 23:01:00 +0530

    added testStyle.js

---
+++ source/styles/testStyle.js
@@ -0,0 +1,21 @@
+// textStyles.js
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: "#34495e",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#7f8c8d",
+    marginBottom: 8,
+  },
+  smallText: {
+    fontSize: 14,
+    color: "#95a5a6",
+  },
+});
