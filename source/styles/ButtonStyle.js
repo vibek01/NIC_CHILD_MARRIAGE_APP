ab9cb0a31dd849253e54b50e14f60a9fdeca2a42
vibek01 <vibekbin305@gmail.com>
2025-07-13 23:00:27 +0530

    modified and added new styles to the ButtonStyle.js

--- source/styles/ButtonStyle.js
+++ source/styles/ButtonStyle.js
@@ -1,28 +1,64 @@
 // buttons.js
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 export default StyleSheet.create({
   primaryButton: {
     backgroundColor: "#3498db",
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 28,
+    borderRadius: 10,
     alignItems: "center",
+    justifyContent: "center",
+    ...(Platform.OS === "ios"
+      ? {
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.2,
+          shadowRadius: 3,
+        }
+      : {
+          elevation: 3,
+        }),
   },
   primaryButtonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
+    color: "#ffffff",
+    fontSize: 17,
+    fontWeight: "600",
   },
+
   secondaryButton: {
     backgroundColor: "#ecf0f1",
-    paddingVertical: 10,
-    paddingHorizontal: 22,
-    borderRadius: 6,
+    paddingVertical: 12,
+    paddingHorizontal: 26,
+    borderRadius: 8,
     alignItems: "center",
+    justifyContent: "center",
+    ...(Platform.OS === "ios"
+      ? {
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 1 },
+          shadowOpacity: 0.1,
+          shadowRadius: 2,
+        }
+      : {
+          elevation: 2,
+        }),
   },
   secondaryButtonText: {
     color: "#2c3e50",
-    fontSize: 15,
+    fontSize: 16,
+  },
+
+  disabledButton: {
+    backgroundColor: "#bdc3c7",
+    paddingVertical: 14,
+    paddingHorizontal: 28,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  disabledButtonText: {
+    color: "#7f8c8d",
+    fontSize: 17,
   },
 });
